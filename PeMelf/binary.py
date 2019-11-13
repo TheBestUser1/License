74802165d9dc0c0ceab9d8f3e70290ebf485e65e
TheBestUser1 <filipcatalinilie@gmail.com>
2019-11-13 11:12:15 -0500

    begging

---
+++ PeMelf/binary.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import r2pipe
+
+
+class Binar:
+    def __init__(self,name):
+            
+        self.r = r2pipe.open(name)
+
+    def find_main(self):
+        print(self.r.cmd("pdf @ main"))
+
+        
+
---
+++ PeMelf/main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from binary import Binar
+
+
+
