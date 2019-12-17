1c0affb3546f432042a0720f597873357b4bec29
TheBestUser1 <filipcatalinilie@gmail.com>
2019-12-17 04:59:37 -0500

    Working on webdesign_ models and finnding a way for techniques
    Signed-off-by: TheBestUser1 <filipcatalinilie@gmail.com>

--- PeMelf/PeMelf_Web/main/models.py
+++ PeMelf/PeMelf_Web/main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Techniques(models.Model):
+    Technique_name = models.CharField(max_length=200)
+    Technique_content = models.TextField()
+
+
+    def __str__(self):
+        return self.Technique_name
+
+
 # Create your models here.
--- PeMelf/PeMelf_Web/main/views.py
+++ PeMelf/PeMelf_Web/main/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Techniques
 
 def homepage(request):
-    return render(request=request,template_name="main/main.html")
+    return render(request=request,
+                    template_name="main/main.html",
+                    context={"Tech":Techniques.objects.all})
 
 
 # Create your views here.
