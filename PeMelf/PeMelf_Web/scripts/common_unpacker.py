4cb6810a7a8511c73164cda3273aeb79269b26c0
TheBestUser1 <filipcatalinilie@gmail.com>
2020-03-31 08:56:46 -0400

    Done the dumping bytes from PE

--- PeMelf/PeMelf_Web/scripts/common_unpacker.py
+++ PeMelf/PeMelf_Web/scripts/common_unpacker.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 #from scripts.r2c import rbin
 from r2c import *
-
+import hashlib
+import os
 
 def find_refs(data):
     i = 1
@@ -22,8 +23,21 @@ def find_function_refs(data,r2):
 def proc_f(refs,r2):
     key = next(iter(refs))
     breakpoint()
+    dumps=[]
     for i in refs[key]:
-        print(i)
+        addr=refs[key][i]['adress'][0]
+        value=refs[key][i]['values'][0]
+        path_to_dump=os.path.join("dumps","{}_{}".format((r2.cmd("i~:1[1]")\
+                                        .split("/")[-1].strip("\n")),addr))
+        bin = r2.cmd("wtf {} {} @ {}".format(path_to_dump,value,addr))
+        name_of_file=''
+        with open(path_to_dump,"rb") as f:
+            file = f.read()
+            hash_of_file = hashlib.md5(file).hexdigest()
+            name_of_file = os.path.join("dumps","{}".format(hash_of_file))
+            os.rename(path_to_dump,name_of_file)
+            dumps.append(name_of_file)
+
     return True
 
 
@@ -39,6 +53,7 @@ def find_bin(r2):
 
         f_refs = r2.find_function(i)
         proc_f(f_refs,r_obj)
+
         #here we treat the function and try to export everything from
         #data section
 
